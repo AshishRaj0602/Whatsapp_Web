698e62221abc24ac065d3617ee8128f1a669401a
Ashish Rajpoot <115613437+AshishRaj0602@users.noreply.github.com>
2023-07-07 13:06:15 +0530

    commit

--- socket/index.js
+++ socket/index.js
@@ -2,7 +2,7 @@ const {Server} =require('socket.io');
 
 const io = new Server(9000, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'https://whatsapp0602.netlify.app/',
     }, 
 })
 
